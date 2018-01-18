69d9de7fd5ef3eef484b38c10b5647351fcc082e
cf8geek <cf8geek@users.noreply.github.com>
2018-01-17 18:37:37 -0600

    Create gather-files.sh
    initial commit

---
+++ gather-files.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/cgi-bin/raw.pl
+wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/cgi-bin/cgi_io.pl
+
+# Note: not grabbing this, because will be customized per unit.
+# wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/html/advanced.html
+
+# Note: this could also come from ""
+wget https://raw.githubusercontent.com/HopeWDM/projector-control_WDM/master/room-controls/javascript/common_pitron-stuff.js
+
+# Note: this could also come from "https://github.com/LutheranChurchOfHope/projector-control/tree/master/javascript"....
+wget https://raw.githubusercontent.com/HopeWDM/projector-control_WDM/master/room-controls/javascript/pitron-vars_wdm.js
+
+# Note: this could also come from ""
+wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/html/javascript/show-hide_pitron.js
+
+# Note: this could also come from ""
+wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/html/javascript/show-hide_mon.js
+
+# Note: this could also come from ""
+wget https://raw.githubusercontent.com/HopeWDM/projector-control_WDM/master/room-controls/stylesheet/common_styles-file.css
+
+# Note: This could also come from "https://raw.githubusercontent.com/LutheranChurchOfHope/projector-control/master/stylesheet/common_styles-file.css"
+wget https://raw.githubusercontent.com/LutheranChurchOfHope/pitron-box/master/html/stylesheet/show-hide_pitron.css
+
